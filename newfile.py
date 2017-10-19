6bbd1e11a4ab8e318eebc17ec4005b21015db8a1
Zhiyuan "Erick" Li <ericklek1994@gmail.com>
2017-10-18 20:42:48 -0500

    Update newfile.py

--- newfile.py
+++ newfile.py
@@ -1,1 +1,13 @@
-print('hello world')
+''' Set the authority and the hub values of all the vertex.
+    vertex.authority = 0
+    vertex.hub = 0
+    
+    Iteration with n steps:
+        Add all the children's authority, assign the value to vertex.authority
+        vertex.authority += children.authority
+        
+        Add all the children's hub, assign the value to vertex.hub
+        vertex.hub += children.hub
+    
+    (Version without normalization.
+'''
